5dbd062e2f595f8edc29723b2f00b6d7e04a6d48
Trisjan <TrisjanM@hotmail.com>
2023-09-15 12:01:46 +0200

    font deleted

--- squad/src/lib/components/Logo.svelte
+++ squad/src/lib/components/Logo.svelte
@@ -2,14 +2,7 @@
 
 <style>
     h1 {
-        font-family: 'Syncopate', sans-serif;
         font-size: 0.8rem;
         text-transform: uppercase;
-    }
-
-    @media (min-width: 38.75rem) {
-        h1 {
-            font-size: 1.2rem;
-        }
     }
 </style>
