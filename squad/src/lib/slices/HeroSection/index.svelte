e2c5bab830ba1b3a7fa6d145719e6f478bfa16f4
moonlightlizaa <eliza.lacroes@gmail.com>
2023-09-15 11:51:45 +0200

    mobile versie hero

--- squad/src/lib/slices/HeroSection/index.svelte
+++ squad/src/lib/slices/HeroSection/index.svelte
@@ -1,4 +1,6 @@
 <script>
+	// @ts-nocheck
+
 	import { PrismicRichText } from '@prismicio/svelte';
 	import { PrismicImage } from '@prismicio/svelte';
 	import Label from './Label.svelte';
@@ -7,22 +9,87 @@
 	export let slice;
 </script>
 
-<section class="container">
-	<PrismicRichText
-		field={slice.primary.content}
-		components={{
-			label: Label
-		}}
-	/>
-
-	<PrismicImage field={slice.primary.hero_img} />
+<section class="hero">
+	<section class="hero-title">
+		<PrismicRichText field={slice.primary.hero_title} />
+	</section>
+	<section class="hero-intro">
+		<PrismicRichText
+			field={slice.primary.content}
+			components={{
+				label: Label
+			}}
+		/>
+	</section>
+	<section class="hero-image">
+		<PrismicImage field={slice.primary.hero_img} />
+	</section>
+	<section class="hero-button">
+		<PrismicRichText field={slice.primary.hero_button} />
+	</section>
 </section>
 
 <style>
-	.container {
-		max-width: 600px;
-		margin: 6em auto;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
-			'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+	@import url('https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Syncopate:wght@700&display=swap');
+	.hero {
+		background-color: #221f29;
+		color: #ffffff;
+	}
+
+	.hero-title {
+		/* font settings: */
+		font-family: Syncopate;
+		font-size: 1.5rem;
+		font-style: normal;
+		font-weight: 700;
+		line-height: normal;
+		letter-spacing: 0.015rem;
+		/* margin + padding settings: */
+		margin: 1rem 0rem 2rem 1rem;
+	}
+
+	.hero-intro {
+		/* font settings: */
+		font-family: Syncopate;
+		font-size: 0.75rem;
+		font-style: normal;
+		font-weight: 700;
+		line-height: 161.102%; /* 1.20825rem */
+		letter-spacing: 0.04125rem;
+		/* margin + padding settings: */
+		margin-bottom: 1rem;
+		margin-left: 1rem;
+	}
+
+	.hero-image {
+		display: none;
+	}
+
+	.hero-button {
+		/* font settings: */
+		color: #3f3e8a;
+		font-family: Poppins;
+		font-size: 1rem;
+		font-style: normal;
+		font-weight: 700;
+		line-height: normal;
+		letter-spacing: 0.00875rem;
+		text-align: center;
+		/* margin + padding settings: */
+		margin-left: 1rem;
+		padding: 0.5rem;
+		background-color: #ffffff;
+		border-radius: 0.375rem;
+		width: 12rem;
+	}
+
+	@media (min-width: 31em) {
+	}
+
+	@media (min-width: 45em) {
+	}
+
+	@media (min-width: 68em) {
 	}
 </style>
