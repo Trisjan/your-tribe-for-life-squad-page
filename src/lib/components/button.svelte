feb3ce9f73605b029f39585dcc8291c2c9c5bd9f
Emona Santiago <emonasantiago1207@gmail.com>
2023-09-15 11:59:08 +0200

    button Component

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,16 @@
+<a href="/" class="button">BEKIJK DE SQUADS</a>
+
+<style>
+    .button {
+  background-color: #fff;
+  color: #3F3E8A;
+  padding: 15px 32px;
+  text-align: center;
+  display: inline-block;
+  text-decoration: none;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 16px;
+}
+</style>
